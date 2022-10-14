589192ff7ddb809e9b68e4e01c57ca260420877f
EvanVJHU <112642773+EvanVJHU@users.noreply.github.com>
2022-10-14 18:20:50 -0400

    Moved non-analysis scripts to "Utility Scripts" folder and more work with data_output script

--- FEM Automation/FEM_data_output.m
+++ FEM Automation/FEM_data_output.m
@@ -6,6 +6,7 @@
 % within each nerve.
 % First created 10/12/2022, Evan Vesper, VNEL
 
+geom_tag = 'geom1';
 dset_vest = 'dset3'; % tag for the dataset containing vestibular nerve flow vector field
 sel_vest_dom = 'geom1_imp1_Vestibular_Nerve1_dom'; % selection for vestibular nerve domain
 sel_vest_outlet_bnd = 'geom1_sel6'; % boundary selection for vestibular nerve outlet (cristas for 3 canals and otolith organs)
@@ -20,21 +21,30 @@ sel_utr_crista_bnd = 'geom1_sel11'; % utricle crista
 % selection argument pair means fxn will only return vector field data 
 % within the selection specified (also the only volume it was actually calculated for)
 flow_vest = mpheval(model,{'cc3.vX','cc3.vY','cc3.vY'},'dataset',dset_vest,'selection',sel_vest_dom); 
+flow_crista = mpheval(model,{'cc3.vX','cc3.vY','cc3.vY'},'dataset',dset_vest,'selection',sel_post_crista_bnd);
 % should double check what units these vectors are in compared to global
 % axes
+
+% have coordinates on crista and simplex mesh from mpheval - just need to
+% figure out how to generate points on this simplex mesh that aren't just
+% the vertices already provided
 
 %% Plot vector field for visualization/testing
 figure
 quiver3(flow_vest.p(1,:),flow_vest.p(2,:),flow_vest.p(3,:),...
     flow_vest.d1,flow_vest.d2,flow_vest.d3)
 
+figure
+plot3(flow_crista.p(1,:),flow_crista.p(2,:),flow_crista.p(3,:),'.')
+
 %%
 % c = mphgetcoords(model,<geomtag>,entitytype,<idx>)
 % verts = stream3(X,Y,Z,U,V,W,startX,startY,startZ,step);
 % generate streamline vertices (axon's nodes)
 
-post_crista_points = mphgetcoords(model,sel_post_crista_bnd,"boundary",model.component('comp1').geom('geom1').feature('sel7').entities(2));
+post_crista_points = mphgetcoords(model,geom_tag,"boundary",2); % this is hopelessly not working
 
+%%
 verts = stream3(flow_vest.p(1,:),flow_vest.p(2,:),flow_vest.p(3,:),...
     flow_vest.d1,flow_vest.d2,flow_vest.d3,startX,startY,startZ,step);
 
